da7eb04d281a641cc04d34d41f2d163a737950f0
happycodingmama <happycodingmama@gmail.com>
2024-10-04 12:02:17 -0700

    update statistics

--- routine_analysis.py
+++ routine_analysis.py
@@ -22,7 +22,10 @@ class RoutineAnalysis:
             delta = after - before
             self.deltas.append((delta) * 1000000)
 
-        return statistics.mean(self.deltas)
+        mean_time =  statistics.mean(self.deltas)
+        median_time =  statistics.median(self.deltas)
+        std_time = statistics.stdev(self.deltas) if len(self.deltas) > 1 else 0
+        return  mean_time, median_time, std_time
     
     def clear_deltas(self):
         self.deltas.clear()
@@ -35,18 +38,19 @@ def data_generator(fut, casemaker, sizes, repetitions_list, file_name):
     # Save the results as a backup
     with open(file_name, 'w') as file:
         
+        # headers
+        file. write("Repetitions,Size,Mean,Median,Std\n")
         results = {}
 
         for repetitions in repetitions_list:
                 for size in sizes:
-                    mean_time = experiment.time_routine(fut, casemaker, size, repetitions)
+                    mean_time, median_time, std_time = experiment.time_routine(fut, casemaker, size, repetitions)
          
+                    print(f"Size: {size}, Repetitions: {repetitions}, Mean: {mean_time:.5f}, Median: {median_time:.5f}, SD: {std_time:.5f}")
 
-                    print(f"Size: {size}, Repetitions: {repetitions}, Mean: {mean_time:.5f}")
-
-                    results[(repetitions, size)] = mean_time
+                    results[(repetitions, size)] = (mean_time, median_time, std_time)
                 
-                    file.write(f"{repetitions},{size},{mean_time:.5f}\n")
+                    file.write(f"{repetitions},{size},{mean_time:.5f},{median_time:.5f},{std_time:.5f}\n")
                 
                     experiment.clear_deltas()    
 
@@ -54,22 +58,25 @@ def data_generator(fut, casemaker, sizes, repetitions_list, file_name):
 
 def main():
 
-    routines = {}
-
     # input sizes
+    small_sizes = [10, 11, 12, 13, 14, 15, 16, 17, 20, 30, 50]
+    sm_mid_sizes = [10, 30, 50, 70, 90, 100, 110, 130, 150, 170, 200, 230, 250, 270, 290, 300]
+    mid_sizes = [10, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 900, 1000]
     sizes = [10, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000, 4000, 7000, 10000, 30000,  50000, 100000, 200000]
-    smaller_sizes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 20]
+    large_sizes = [500000, 1000000, 2000000, 4000000, 10000000]
 
     # number of samples
-    lower_repetitions_list = [20, 50, 70, 90, 100]
-    repetitions_list = [50, 100, 500, 1000, 1500]
+    small_samples = [20, 50, 70, 90, 100]
+    mid_samples = [50, 100, 500, 1000, 2000, 5000]
+    samples = [5000, 10000, 30000, 50000, 100000]
 
     # Run the experiments
-    data_generator(routine_1.fut, routine_1.casemaker, sizes, repetitions_list, "routine_1.txt")
-    data_generator(routine_3.fut, routine_3.casemaker, sizes, repetitions_list, "routine_3.txt")
-    data_generator(routine_4.fut, routine_4.casemaker, sizes, repetitions_list, "routine_4.txt")
-    data_generator(routine_5.fut, routine_5.casemaker, sizes, repetitions_list, "routine_5.txt")
-    data_generator(routine_2.fut, routine_2.casemaker, smaller_sizes, lower_repetitions_list, "routine_2.txt")
+    data_generator(routine_1.fut, routine_1.casemaker, large_sizes, samples, "routine_1.txt")
+    data_generator(routine_2.fut, routine_2.casemaker, small_sizes, [300, 500, 1000], "routine_2.txt")
+    data_generator(routine_3.fut, routine_3.casemaker, [3000, 5000, 10000], mid_samples, "routine_3.txt")
+    data_generator(routine_4.fut, routine_4.casemaker, sm_mid_sizes, [500, 1000, 2000, 5000], "routine_4.txt")
+    data_generator(routine_5.fut, routine_5.casemaker, sizes, mid_samples, "routine_5.txt")
+   
 
     print("Done")
 
